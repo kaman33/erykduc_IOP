6d51957ac4707da122ca9473203a5a743f4adcb3
erykduc <erykduc@gmail.com>
2025-11-20 20:27:11 +0100

    Wersja 2 - sprawdzenie czy liczba jest doskonala

--- liczba.cpp
+++ liczba.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 using namespace std;
+
+bool czy_doskonala(int n){
+
+	int suma = 0;
+
+	for ( int i = 1; i < n; i++ ){
+		if ( n % i == 0 ) suma += i;
+	}
+	return suma == n;
+}
+
 int main(){
 
 	int n;
 	cin >> n;
 	cout << n;
+	if(czy_doskonala(n))
+		cout << "jest doskonala\n";
+	else
+		cout << "nie jest doskonala\n";
 
 	return 0;
 }
