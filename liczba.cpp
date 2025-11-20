e51c58b249d52a8e358dc5d58c7ecf5dd507fb39
erykduc <erykduc@gmail.com>
2025-11-20 20:29:14 +0100

    Dodanie sprawdzenia czy liczba jest dodatnia

--- liczba.cpp
+++ liczba.cpp
@@ -21,5 +21,14 @@ int main(){
 	else
 		cout << "nie jest doskonala\n";
 
-	return 0;
+int main() {
+    int x;
+    cin >> x;
+    if (x <= 0) {
+        cout << "Blad: liczba musi byc dodatnia.\n";
+        return 0;
+    }
+
+    cout << x << endl;
 }
+
